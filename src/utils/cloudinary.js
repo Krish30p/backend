38585924b500af1694d70bcdfdbca08c251d9024
KrishnaPatel <kp06krish@gmail.com>
2025-09-30 16:55:20 +0530

    add register controller and bug fixes

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -18,7 +18,8 @@ cloudinary.config({
                 resource_type: "auto"
             })
             // file has been uploaded successfully 
-            console.log("file is uploaded on cloudinary". response.url);
+            console.log("file is uploaded on cloudinary", response.url);
+            return response;
 
         } catch (error) {
             fs.unlinkSync(localFilePath)
